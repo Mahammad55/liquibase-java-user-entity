62c41c59134c5fc20e2a7d018b7da0dc7ec3418d
Mahammad Ilyazov <ilyazovmehemmed@gmail.com>
2024-02-12 12:44:30 +0400

    added user entity

---
+++ src/main/java/az/ingress/liquibasejavauserentity/entity/User.java
@@ -0,0 +1,37 @@
+package az.ingress.liquibasejavauserentity.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String surname;
+
+    private Integer age;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+}
