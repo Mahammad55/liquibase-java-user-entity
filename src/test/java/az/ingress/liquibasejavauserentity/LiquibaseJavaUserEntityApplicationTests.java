52a76730a698d437d7a34f95863a71826679740b
Mahammad Ilyazov <ilyazovmehemmed@gmail.com>
2024-02-12 12:27:24 +0400

    connected to postgres database

---
+++ src/test/java/az/ingress/liquibasejavauserentity/LiquibaseJavaUserEntityApplicationTests.java
@@ -0,0 +1,13 @@
+package az.ingress.liquibasejavauserentity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiquibaseJavaUserEntityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
